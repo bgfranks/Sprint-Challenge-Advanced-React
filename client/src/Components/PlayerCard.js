9fe0b99f2a7326880b9fd1d5a46664a130fe17d3
Brandon Franks <bgfranks106@icloud.com>
2020-01-24 22:46:43 -0500

    wrote a test for checking if the card container displays

--- client/src/Components/PlayerCard.js
+++ client/src/Components/PlayerCard.js
@@ -3,7 +3,7 @@ import "./PlayerCard.css";
 
 const PlayerCard = ({ name, country }) => {
   return (
-    <div className="card-container">
+    <div className="card-container" data-testid="card-container">
       <h3>{name}</h3>
       <p>{country}</p>
     </div>
