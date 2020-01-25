1466261eda884e375d0466f480db33950ddaa8f5
Brandon Franks <bgfranks106@icloud.com>
2020-01-24 21:17:59 -0500

    setup the base files for the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    playerArray: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(players => {
+        console.log(players);
+        this.setState({ ...this.state, playerArray: players });
+        // console.log(user.login)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>text</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
